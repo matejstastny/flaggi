a78e45ecb9290f7ebbd2aafc4e51ce6ce3638d58
Matěj Šťastný <matysta@outlook.com>
2025-04-04 09:03:26 -0700

    Added menu to lobby transtition

--- app/client/src/main/java/flaggi/client/App.java
+++ app/client/src/main/java/flaggi/client/App.java
@@ -24,102 +24,105 @@ import flaggi.shared.util.ScreenUtil;
 
 public class App {
 
-    private final GPanel gpanel;
+	private final GPanel gpanel;
 
-    // Main ---------------------------------------------------------------------
+	// Main ---------------------------------------------------------------------
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(App::new);
-    }
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(App::new);
+	}
 
-    public App() {
-        initializeLogger();
-        this.gpanel = getDefaultGpanel();
-        addDefaultWidgets();
-        gotoLobby();
-    }
+	public App() {
+		initializeLogger();
+		this.gpanel = getDefaultGpanel();
+		addDefaultWidgets();
+		gotoMainMenu();
+	}
 
-    // Static -------------------------------------------------------------------
+	// Static -------------------------------------------------------------------
 
-    public static void handleFatalError() {
-        Logger.log(LogLevel.ERROR, "FATAL ERROR DETECTED! SHUTTING DOWN...");
-        System.exit(1);
-    }
+	public static void handleFatalError() {
+		Logger.log(LogLevel.ERROR, "FATAL ERROR DETECTED! SHUTTING DOWN...");
+		System.exit(1);
+	}
 
-    // Events -------------------------------------------------------------------
+	// Events -------------------------------------------------------------------
 
-    public void gotoMainMenu() {
-        toggleUi(UiTags.MAIN_MENU);
-    }
+	public void gotoMainMenu() {
+		toggleUi(UiTags.MAIN_MENU);
+	}
 
-    public void gotoLobby() {
-        toggleUi(UiTags.LOBBY);
+	public void gotoLobby() {
+		toggleUi(UiTags.LOBBY);
 
-        Map<Integer, String> clients = new HashMap<Integer, String>();
-        for (int i = 0; i <= 40; i++) {
-            clients.put(i, "Client:" + i);
-        }
-        this.gpanel.getWidgetsOfClass(LobbyUi.class).forEach(x -> x.setClients(clients));
-        this.gpanel.toggleWidgetsVisibility(false);
-        this.gpanel.toggleTaggedWidgetsVisibility(UiTags.LOBBY, true);
-    }
+		Map<Integer, String> clients = new HashMap<Integer, String>();
+		for (int i = 0; i <= 40; i++) {
+			clients.put(i, "Client:" + i);
+		}
+		this.gpanel.getWidgetsOfClass(LobbyUi.class).forEach(x -> x.setClients(clients));
+		this.gpanel.toggleWidgetsVisibility(false);
+		this.gpanel.toggleTaggedWidgetsVisibility(UiTags.LOBBY, true);
+	}
 
-    public String joinServer(String name, String ip) {
-        Logger.log(LogLevel.DEBUG, "Join server button pressed.");
-        setConfigField("username", name);
-        setConfigField("server.ip", ip);
-        return "Connecting...";
-    }
+	// External events ----------------------------------------------------------
 
-    public void invitePlayer(String username, Integer id) {
-        Logger.log(LogLevel.DEBUG, username + " " + id);
-    }
+	public String joinServer(String name, String ip) {
+		Logger.log(LogLevel.DEBUG, "Join server button pressed.");
+		setConfigField("username", name);
+		setConfigField("server.ip", ip);
+		gotoLobby();
+		return "Connecting...";
+	}
 
-    // Config handeling ---------------------------------------------------------
+	public void invitePlayer(String username, Integer id) {
+		Logger.log(LogLevel.DEBUG, username + " " + id);
+	}
 
-    private void setConfigField(String key, String val) {
-        try {
-            Constants.CONFIG.setField(key, val);
-        } catch (IOException e) {
-            Logger.log(LogLevel.ERROR, "An error occured while setting property value", e);
-            handleFatalError();
-        }
-    }
+	// Config handeling ---------------------------------------------------------
 
-    // Private ------------------------------------------------------------------
+	private void setConfigField(String key, String val) {
+		try {
+			Constants.CONFIG.setField(key, val);
+		} catch (IOException e) {
+			Logger.log(LogLevel.ERROR, "An error occured while setting property value", e);
+			handleFatalError();
+		}
+	}
 
-    private void initializeLogger() {
-        Logger.setLogFile(Constants.LOG_FILE);
-        Logger.setLogLevelsToIgnore(Constants.IGNORED_LOG_LEVES);
-        Logger.log(LogLevel.INFO, "Application start.");
-        if (Constants.LOG_MEM_USAGE) {
-            Logger.logMemoryUsage(Constants.MEM_LOG_INTERVAL_SEC);
-        }
-    }
+	// Private ------------------------------------------------------------------
 
-    private GPanel getDefaultGpanel() {
-        int[] screenSize = ScreenUtil.getScreenDimensions();
-        GPanel gp = new GPanel(screenSize[0], screenSize[1], Constants.WINDOW_RESIZABLE, Constants.WINDOW_NAME);
-        if (Constants.FRAMERATE >= 0) {
-            gp.setFpsCap(Constants.FRAMERATE);
-        }
-        gp.setIconOSDependend(Constants.ICON_WIN, Constants.ICON_MAC, Constants.ICON_WIN, Constants.ICON_WIN);
-        return gp;
-    }
+	private void initializeLogger() {
+		Logger.setLogFile(Constants.LOG_FILE);
+		Logger.setLogLevelsToIgnore(Constants.IGNORED_LOG_LEVES);
+		Logger.log(LogLevel.INFO, "Application start.");
+		if (Constants.LOG_MEM_USAGE) {
+			Logger.logMemoryUsage(Constants.MEM_LOG_INTERVAL_SEC);
+		}
+	}
 
-    private void addDefaultWidgets() {
-        this.gpanel.add( //
-                new MenuScreen(Constants.MENU_NAME_FIELD, Constants.MENU_IP_FIELD, this::joinServer), //
-                new MenuBackground(), //
-                new LobbyUi(this::invitePlayer));
-        this.gpanel.toggleWidgetsVisibility(false);
-    }
+	private GPanel getDefaultGpanel() {
+		int[] screenSize = ScreenUtil.getScreenDimensions();
+		GPanel gp = new GPanel(screenSize[0], screenSize[1], Constants.WINDOW_RESIZABLE, Constants.WINDOW_NAME);
+		if (Constants.FRAMERATE >= 0) {
+			gp.setFpsCap(Constants.FRAMERATE);
+		}
+		gp.setIconOSDependend(Constants.ICON_WIN, Constants.ICON_MAC, Constants.ICON_WIN, Constants.ICON_WIN);
+		return gp;
+	}
 
-    private void toggleUi(String... tags) {
-        this.gpanel.toggleWidgetsVisibility(false);
-        for (String tag : tags) {
-            this.gpanel.toggleTaggedWidgetsVisibility(tag, true);
-        }
-    }
+	private void addDefaultWidgets() {
+		this.gpanel.add( //
+				new MenuScreen(Constants.MENU_NAME_FIELD, Constants.MENU_IP_FIELD, this::joinServer), //
+				new MenuBackground(), //
+				new LobbyUi(this::invitePlayer));
+		this.gpanel.toggleWidgetsVisibility(false);
+	}
+
+	private void toggleUi(String... tags) {
+		this.gpanel.toggleWidgetsVisibility(false);
+		for (String tag : tags) {
+			this.gpanel.toggleTaggedWidgetsVisibility(tag, true);
+		}
+	}
 
 }
