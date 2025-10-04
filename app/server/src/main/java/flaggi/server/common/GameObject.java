cbd1d97191d5a418bedc2bbde7b51fc73b0a6966
Matej Stastny <matysta@outlook.com>
2025-10-04 11:32:02 -0700

    Added a game object class

---
+++ app/server/src/main/java/flaggi/server/common/GameObject.java
@@ -0,0 +1,79 @@
+// ------------------------------------------------------------------------------
+// GameObject.java - Stores data about game objects
+// ------------------------------------------------------------------------------
+// Author: Matej Stastny
+// Date: 06-21-2025 (MM-DD-YYYY)
+// License: MIT
+// Link: https://github.com/matejstastny/flaggi
+// ------------------------------------------------------------------------------
+
+package flaggi.server.common;
+
+import flaggi.proto.ServerMessages.ServerGameObject;
+import flaggi.proto.ServerMessages.GameObjectType;
+
+public class GameObject {
+
+	private GameObjectType type;
+	private Hitbox collision;
+	private double x, y, hp;
+
+	// Constructors -------------------------------------------------------------
+
+	public GameObject(GameObjectType type, double x, double y, Hitbox c) {
+		this.collision = c;
+		this.type = type;
+		this.x = x;
+		this.y = y;
+	}
+
+	// Public -------------------------------------------------------------------
+
+	public ServerGameObject toProto() {
+		return ServerGameObject.newBuilder().setType(type).setX(x).setY(y).setCollX(collision.getX()).setCollY(collision.getY()).setCollWidth(collision.getWidth()).setCollHeight(collision.getHeight()).build();
+	}
+
+	// Accesors -----------------------------------------------------------------
+
+	public GameObjectType type() {
+		return type;
+	}
+
+	public Hitbox collision() {
+		return collision;
+	}
+
+	public double x() {
+		return x;
+	}
+
+	public double y() {
+		return y;
+	}
+
+	public double hp() {
+		return hp;
+	}
+
+	// Modifiers ----------------------------------------------------------------
+
+	public void setType(GameObjectType type) {
+		this.type = type;
+	}
+
+	public void setCollision(Hitbox c) {
+		this.collision = c;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public void setHp(double hp) {
+
+	}
+}
