0ece1dc0e3f387cd0f76b84168fb4de0b0f80b1a
Matej Stastny <matysta@outlook.com>
2025-10-04 11:31:33 -0700

    Added a hitbox constants class

---
+++ app/server/src/main/java/flaggi/server/constants/Hitboxes.java
@@ -0,0 +1,27 @@
+// ------------------------------------------------------------------------------
+// Hitboxes.java - Constant hitbox values
+// ------------------------------------------------------------------------------
+// Author: Matej Stastny
+// Date: 12/12/2024 (MM-DD-YYYY)
+// License: MIT
+// Link: https://github.com/matejstastny/flaggi
+// ------------------------------------------------------------------------------
+
+package flaggi.server.constants;
+
+import flaggi.server.common.Hitbox;
+
+public class Hitboxes {
+
+	// Private constructor to prevent instantiation
+	private Hitboxes() {
+		throw new UnsupportedOperationException("Hitboxes is a constants class and cannot be instantiated.");
+	}
+
+	// Hitboxes ----------------------------------------------------------------
+
+	public static Hitbox player() {
+		return new Hitbox(-2.5, -2.5, 5, 5);
+	}
+
+}
