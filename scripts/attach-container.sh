090ef890b9b22bd72a6f15fa5ff7af5cfe2efb2f
Matej Stastny <matysta@outlook.com>
2025-11-26 18:21:08 +0000

    Added a script to attach to the devcontainer from a terminal

---
+++ scripts/attach-container.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# --------------------------------------------------------------------------------------------
+# attach-container.sh - attaches to vscode devcontainer
+# --------------------------------------------------------------------------------------------
+# Author: Matej Stastny
+# Date: 2025-11-23 (YYYY-MM-DD)
+# License: MIT
+# Link: https://github.com/matejstastny/flaggi
+# --------------------------------------------------------------------------------------------
+
+source "$(dirname "$0")/logging.sh"
+
+CONTAINER_ID=$(
+    docker ps --format "{{.ID}} {{.Image}}" |
+        grep "^.* vsc-flaggi" |
+        awk '{print $1}' |
+        head -n 1
+)
+
+if [ -z "$CONTAINER_ID" ]; then
+    log error "No running dev container found for flaggi"
+    log error "Make sure VS Code has reopened the project in the container"
+    exit 1
+fi
+
+log info "Attaching to container: $CONTAINER_ID"
+docker exec -it -u flaggi "$CONTAINER_ID" bash -l
