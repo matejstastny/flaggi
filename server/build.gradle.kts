d845c758015ca5488a89d476ba6cc2d0057df852
Matej Stastny <matysta@outlook.com>
2026-02-21 20:05:17 +0000

    Added a database

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -5,17 +5,22 @@ plugins {
     id("com.google.protobuf") version "0.9.5"
 }
 
-dependencies { implementation(project(":shared")) }
+dependencies {
+    implementation(project(":shared"))
+    implementation("org.xerial:sqlite-jdbc:3.45.3.0")
+    implementation("io.javalin:javalin:6.7.0")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+}
 
 application { mainClass.set("flaggi.server.Server") }
 
 tasks.shadowJar {
-  destinationDirectory = rootProject.layout.projectDirectory.dir("shadowjar")
-  archiveBaseName = "flaggi-server"
-  archiveClassifier = ""
-  from(rootProject.layout.projectDirectory.file("LICENSE")) {
-    into("licenses")
-  }
+    destinationDirectory = rootProject.layout.projectDirectory.dir("shadowjar")
+    archiveBaseName = "flaggi-server"
+    archiveClassifier = ""
+    from(rootProject.layout.projectDirectory.file("LICENSE")) {
+        into("licenses")
+    }
 }
 
 tasks.jar {
